4ce9c11c01d9e070e672b464b96b9cda14a1e071
Dmitriy217 <64257802+Dmitriy217@users.noreply.github.com>
2024-03-16 13:41:35 +0500

    Update script.js

--- script.js
+++ script.js
@@ -13,10 +13,10 @@ let sumpay = document.querySelector(".sumpu p");
 sumpay.innerHTML = pay.value * 500 + "р - платные услуги";	
 
 let notb = document.querySelector(".notbonus p");
-notb.innerHTML = pay.value * 500 + free.value * 320 + "р - без премии 10к";
+notb.innerHTML = pay.value * 500 + free.value * 320 + "р - без премии 15к";
 
 let wb = document.querySelector(".withbonus p");
-wb.innerHTML = 10000 + pay.value * 500 + free.value * 320 + "р - с премии 10к";
+wb.innerHTML = 15000 + pay.value * 500 + free.value * 320 + "р - с премией 15к";
 
 
 
@@ -29,10 +29,10 @@ let sumfree = document.querySelector(".sumbz p");
 sumfree.innerHTML = free.value * 320 + "р - бесплатные замены";	
 
 let notb = document.querySelector(".notbonus p");
-notb.innerHTML = pay.value * 500 + free.value * 320 + "р - без премии 10к";
+notb.innerHTML = pay.value * 500 + free.value * 320 + "р - без премии 15к";
 
 let wb = document.querySelector(".withbonus p");
-wb.innerHTML = 10000 + pay.value * 500 + free.value * 320 + "р - с премии 10к";
+wb.innerHTML = 15000 + pay.value * 500 + free.value * 320 + "р - с премией 15к";
 
 }
 
