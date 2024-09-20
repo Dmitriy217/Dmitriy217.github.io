44ce367904fda47e69c1b2d35a0ccc798ec9833d
Dmitriy217 <64257802+Dmitriy217@users.noreply.github.com>
2024-09-20 11:35:32 +0500

    script.js

--- script.js
+++ script.js
@@ -10,13 +10,13 @@ let free = document.querySelector("#bz");
 pay.onchange = () => {
 
 let sumpay = document.querySelector(".sumpu p");
-sumpay.innerHTML = pay.value * 500 + "р - платные услуги";	
+sumpay.innerHTML = pay.value * 550 + "р - платные услуги";	
 
 let notb = document.querySelector(".notbonus p");
-notb.innerHTML = pay.value * 500 + free.value * 320 + "р - без премии 15к";
+notb.innerHTML = pay.value * 550 + free.value * 320 + "р - без премии 15к";
 
 let wb = document.querySelector(".withbonus p");
-wb.innerHTML = 15000 + pay.value * 500 + free.value * 320 + "р - с премией 15к";
+wb.innerHTML = 15000 + pay.value * 550 + free.value * 320 + "р - с премией 15к";
 
 
 
@@ -29,10 +29,10 @@ let sumfree = document.querySelector(".sumbz p");
 sumfree.innerHTML = free.value * 320 + "р - бесплатные замены";	
 
 let notb = document.querySelector(".notbonus p");
-notb.innerHTML = pay.value * 500 + free.value * 320 + "р - без премии 15к";
+notb.innerHTML = pay.value * 550 + free.value * 320 + "р - без премии 15к";
 
 let wb = document.querySelector(".withbonus p");
-wb.innerHTML = 15000 + pay.value * 500 + free.value * 320 + "р - с премией 15к";
+wb.innerHTML = 15000 + pay.value * 550 + free.value * 320 + "р - с премией 15к";
 
 }
 
